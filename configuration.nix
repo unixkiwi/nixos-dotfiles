567294c64acf6bec7b22df71458099c6e4e37722
unixkiwi <unixkiwi77@outlook.com>
2025-12-27 15:07:22 +0100

    feat: add nvidia

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,20 @@
   # Console keymap
   console.keyMap = "de-latin1";
 
+  # NVIDIA
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      open = false;
+      modesetting.enable = true;
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+
   # User
   users.users.kiwi = {
     isNormalUser = true;

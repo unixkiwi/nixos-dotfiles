83db4df449faa76144161f7f2a388c068e8d6dc6
unixkiwi <unixkiwi77@outlook.com>
2025-12-29 19:36:32 +0100

    feat: add nbfc

---
+++ hosts/laptop/modules/system/nbfc.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  cfg = pkgs.writeText "nbfc.json" (
+    builtins.toJSON {
+      SelectedConfigId = "Acer Aspire VN7-792G V17 Nitro BE";
+    }
+  );
+in
+{
+  environment.systemPackages = [
+    pkgs.nbfc-linux
+  ];
+
+  systemd.services.nbfc_service = {
+    enable = true;
+    description = "NoteBook FanControl service";
+    serviceConfig.Type = "simple";
+    path = [ pkgs.kmod ];
+
+    script = "${pkgs.nbfc-linux}/bin/nbfc_service --config-file '${cfg}'";
+
+    wantedBy = [ "multi-user.target" ];
+  };
+}

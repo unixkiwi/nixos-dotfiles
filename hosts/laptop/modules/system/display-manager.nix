164bcb6e3d31b0b1888948a00efc4f44e633d836
unixkiwi <unixkiwi77@outlook.com>
2025-12-30 15:42:42 +0100

    fix: sddm

--- hosts/laptop/modules/system/display-manager.nix
+++ hosts/laptop/modules/system/display-manager.nix
@@ -1,10 +1,35 @@
-{ inputs, pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   # Display Manager
+  /*
+    services.displayManager = {
+      enable = true;
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+        theme = "sddm-astronaut-theme";
+        extraPackages = [ pkgs.sddm-astronaut ];
+      };
+    };
+  */
   services.displayManager.sddm = {
     enable = true;
+    package = lib.mkForce pkgs.kdePackages.sddm;
+    extraPackages = with pkgs; [
+      sddm-astronaut
+      kdePackages.qt5compat
+    ];
     wayland.enable = true;
     theme = "sddm-astronaut-theme";
-    extraPackages = [ pkgs.sddm-astronaut ];
+    enableHidpi = true;
+    settings = {
+      General = {
+        DisplayServer = "wayland";
+      };
+    };
   };
+
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut
+  ];
 }

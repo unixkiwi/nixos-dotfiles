8afdcb7e44c7adc15c8d5c122e73a9614e6874d5
unixkiwi <unixkiwi77@outlook.com>
2026-01-04 19:13:33 +0100

    feat: custom override for gruvbox

--- hosts/laptop/modules/nixos/theme.nix
+++ hosts/laptop/modules/nixos/theme.nix
@@ -4,6 +4,15 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    override = {
+      base00 = "#191c1e";
+      base01 = "#383432";
+      base02 = "#4c4541";
+      base03 = "#624f50";
+      base04 = "#afaa8f";
+      base05 = "#d1c09e";
+      base06 = "#e9d8ad";
+    };
     /*
       fonts = {
         monospace.package = pkgs.nerd-fonts.caskaydia-cove;

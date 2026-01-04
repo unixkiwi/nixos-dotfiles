92dc8ee4374978973b4b5b4c6c4159d900491cd7
unixkiwi <unixkiwi77@outlook.com>
2026-01-04 19:13:18 +0100

    feat: add vscode

--- hosts/laptop/modules/development/default.nix
+++ hosts/laptop/modules/development/default.nix
@@ -1,7 +1,15 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     ./android.nix
     ./git.nix
   ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dart-code.flutter
+      christian-kohler.path-intellisense
+    ];
+  };
 }
